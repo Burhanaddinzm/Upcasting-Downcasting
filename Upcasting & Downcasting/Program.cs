c9f8a160788ed8825d4da4ed91a97a5a44c13c24
Burhanaddinzm <burhanaddinzm@code.edu.az>
2023-10-29 16:11:48 +0400

    Upcasting & Downcasting

---
+++ Upcasting & Downcasting/Program.cs
@@ -0,0 +1,10 @@
+﻿//Upcasting
+using Upcasting___Downcasting;
+
+Person person = new Teacher();
+person.Name = "Test";
+//person.Salary; Doesn't work
+
+//Downcasting
+Teacher teacher = (Teacher)person;// or Teacher teacher = person as Teahcer;
+Person[] people = { new Teacher(), new Student() };
---
+++ Upcasting & Downcasting/Teacher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Upcasting___Downcasting
+{
+    internal class Teacher : Person
+    {
+        public int Salary { get; set; }
+    }
+}
